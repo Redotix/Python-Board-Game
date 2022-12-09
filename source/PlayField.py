baf42575af87e855d3e4bb1947286f221c93b342
Redotix99 <hlivakmatej@gmail.com>
2022-12-09 01:15:23 +0100

    Began working on dynamic field generation based on user input

--- source/PlayField.py
+++ source/PlayField.py
@@ -1,15 +1,53 @@
 class PlayField:
-    playingFieldVar = []
     fieldTiles = []
+    canvassize = (5, 5)
+    dotsize = 70
+    fieldincrement = 8
+    fieldminsize = 16
+    canvasminsize = 5
+    border = 2
+    maxdotsize = 70
     color = None
 
-    def __init__(self, fieldcolor, inputfield):
-        self.playingFieldVar = inputfield
+    def __init__(self, fieldcolor):
         self.color = fieldcolor
         self.fieldTiles = []
-        for i in range(len(self.playingFieldVar)):
-            tile = self.Tile(i, self.playingFieldVar[i])
-            self.fieldTiles.append(tile)
+        # for i in range(len(self.playingFieldVar)):
+        #     tile = self.Tile(i, self.playingFieldVar[i])
+        #     self.fieldTiles.append(tile)
+
+    def generategield(self, extratiles):
+        canvassize = self.canvasminsize + (extratiles * 2) + self.border
+        self.dotsize = self.maxdotsize / (canvassize / 7)
+        self.canvassize = (canvassize, canvassize)
+        center = (canvassize / 2) - 0.5
+
+        # Creating the 4 inner corners of the playing field.
+        self.fieldTiles.append((center + 1, center + 1))
+        self.fieldTiles.append((center - 1, center + 1))
+        self.fieldTiles.append((center + 1, center - 1))
+        self.fieldTiles.append((center - 1, center - 1))
+
+        # creating extra tiles from the 4 inner corners and extending them outwards.
+        for x in range(1 + extratiles):
+            self.fieldTiles.append((center + 2 + x, center + 1))
+            self.fieldTiles.append((center - 2 - x, center - 1))
+
+            self.fieldTiles.append((center + 2 + x, center - 1))
+            self.fieldTiles.append((center - 2 - x, center + 1))
+
+        for y in range(1 + extratiles):
+            self.fieldTiles.append((center + 1,  center - 2 - y))
+            self.fieldTiles.append((center - 1, center + 2 + y))
+
+            self.fieldTiles.append((center - 1, center - 2 - y))
+            self.fieldTiles.append((center + 1, center + 2 + y))
+
+        # Adding last 4 outer tiles to close off the track loop
+        self.fieldTiles.append((center + extratiles + 2, center))
+        self.fieldTiles.append((center - extratiles - 2, center))
+        self.fieldTiles.append((center, center + extratiles + 2))
+        self.fieldTiles.append((center, center - extratiles - 2))
 
     class Tile:
         tileCoords = ()
--- source/main.py
+++ source/main.py
@@ -1,48 +1,67 @@
+from Settings import *
 from Renderer import *
 from PlayField import *
 from Player import *
 
-import time
 
-CanvasSize = (13, 13)
+def inputsettings():
+    playeramount = 4
+    pieceamount = 4
+    extratiles = 3
+    return Settings(playeramount, pieceamount, extratiles)
+
+
+settings = inputsettings()
+print(settings.playeramount, settings.pieceamount, settings.extratiles)
+
+playingfield = PlayField("white")
+playingfield.generategield(settings.extratiles)
+
+canvassize = playingfield.canvassize
 win = t.Screen()
 win.setup(600, 600)
-win.setworldcoordinates(0, CanvasSize[0], CanvasSize[1], 0)
+win.setworldcoordinates(0, canvassize[0], canvassize[1], 0)
 win.bgcolor("grey")
 
 canvas = win.getcanvas()
 
+# Debug rendering the playing field.
+t.tracer(False)
+for i in range(len(playingfield.fieldTiles)):
+    Renderer().render("white", playingfield.fieldTiles[i], playingfield.dotsize)
+t.tracer(True)
+
+
 # Variable with the playing field color and Coordinates in Touples
-playingField = PlayField("white",
-                         [(7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (8, 5), (9, 5), (10, 5), (11, 5), (11, 6), (11, 7),
-                          (10, 7), (9, 7), (8, 7), (7, 7), (7, 8), (7, 9), (7, 10), (7, 11), (6, 11), (5, 11), (5, 10),
-                          (5, 9), (5, 8), (5, 7), (4, 7), (3, 7), (2, 7), (1, 7), (1, 6), (1, 5), (2, 5), (3, 5),
-                          (4, 5), (5, 5), (5, 4), (5, 3), (5, 2), (5, 1), (6, 1), ])
+# playingField = PlayField("white",
+#                          [(7, 1), (7, 2), (7, 3), (7, 4), (7, 5), (8, 5), (9, 5), (10, 5), (11, 5), (11, 6), (11, 7),
+#                           (10, 7), (9, 7), (8, 7), (7, 7), (7, 8), (7, 9), (7, 10), (7, 11), (6, 11), (5, 11), (5, 10),
+#                           (5, 9), (5, 8), (5, 7), (4, 7), (3, 7), (2, 7), (1, 7), (1, 6), (1, 5), (2, 5), (3, 5),
+#                           (4, 5), (5, 5), (5, 4), (5, 3), (5, 2), (5, 1), (6, 1), ])
 
 # Variables that define the players and their team color, ending house coordinates, starting house coordinates
 # starting PlayField tile, ending PlayField tile and the color of the piece.
-Player1 = Player("darkBlue", [(6, 2), (6, 3), (6, 4), (6, 5), ],
-                 [(10, 1), (11, 1), (10, 2), (11, 2), ], 0, 39, "blue", 4)
-Player2 = Player("gold", [(6, 10), (6, 9), (6, 8), (6, 7), ],
-                 [(1, 10), (1, 11), (2, 10), (2, 11), ], 20, 19, "yellow", 4)
-Player3 = Player("darkGreen", [(10, 6), (9, 6), (8, 6), (7, 6), ],
-                 [(10, 10), (10, 11), (11, 10), (11, 11), ], 10, 9, "green", 4)
-Player4 = Player("maroon", [(2, 6), (3, 6), (4, 6), (5, 6), ],
-                 [(1, 1), (1, 2), (2, 1), (2, 2), ], 30, 29, "red", 4)
-
-players = [Player1, Player2, Player3, Player4]
-
-for player in range(len(players)):
-    for piece in range(len(players[player].playerPieces)):
-        players[player].playerPieces[piece].placepiece(players[player].startCoords[piece])
-
-    Renderer().render(players[player].playerColor, players[player].houseCoords + players[player].startCoords)
-Renderer().render(playingField.color, playingField.playingFieldVar)
-
-for player in range(len(players)):
-    for piece in range(len(players[player].playerPieces)):
-        for tile in range(len(playingField.playingFieldVar)):
-            players[player].playerPieces[piece].movepiece(playingField.playingFieldVar[tile])
-            time.sleep(0.01)
+# Player1 = Player("darkBlue", [(6, 2), (6, 3), (6, 4), (6, 5), ],
+#                  [(10, 1), (11, 1), (10, 2), (11, 2), ], 0, 39, "blue", 4)
+# Player2 = Player("gold", [(6, 10), (6, 9), (6, 8), (6, 7), ],
+#                  [(1, 10), (1, 11), (2, 10), (2, 11), ], 20, 19, "yellow", 4)
+# Player3 = Player("darkGreen", [(10, 6), (9, 6), (8, 6), (7, 6), ],
+#                  [(10, 10), (10, 11), (11, 10), (11, 11), ], 10, 9, "green", 4)
+# Player4 = Player("maroon", [(2, 6), (3, 6), (4, 6), (5, 6), ],
+#                  [(1, 1), (1, 2), (2, 1), (2, 2), ], 30, 29, "red", 4)
+#
+# players = [Player1, Player2, Player3, Player4]
+#
+# for player in range(len(players)):
+#     for piece in range(len(players[player].playerPieces)):
+#         players[player].playerPieces[piece].placepiece(players[player].startCoords[piece])
+#
+#     Renderer().render(players[player].playerColor, players[player].houseCoords + players[player].startCoords)
+# Renderer().render(playingField.color, playingField.playingFieldVar)
+#
+# for player in range(len(players)):
+#     for piece in range(len(players[player].playerPieces)):
+#         for tile in range(len(playingField.playingFieldVar)):
+#             players[player].playerPieces[piece].movepiece(playingField.playingFieldVar[tile])
 
 win.mainloop()
