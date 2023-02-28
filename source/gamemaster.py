d620d36c55ec58ded80136271bec633bf4c75132
Redotix99 <hlivakmatej@gmail.com>
2023-02-28 22:02:20 +0100

    Fixing of the movement logic
    Thank you Petti :)

--- source/gamemaster.py
+++ source/gamemaster.py
@@ -17,10 +17,24 @@ win.bgcolor(bgcolor)
 
 fontsizefactor = 40
 
+GAMESTATE_AWAIT = -1
+GAMESTATE_READY = 0
+GAMESTATE_GETPIECEOUT = 1
+GAMESTATE_ROLL = 2
+GAMESTATE_MOVE = 3
+
+MOVESTATE_SUCCESS = 0
+MOVESTATE_OUTOFBOUNDS = 1
+MOVESTATE_TILEOCCUPIED = 2
+
+gamefont = ('Arial', int(windowsize / fontsizefactor), 'normal')
+
 
 class GameMaster:
     settings = None
     playingfield = PlayField()
+
+    currentGameState = 1
 
     lenghtoftravel = 0
     players = []
@@ -37,10 +51,12 @@ class GameMaster:
         self.playingfield.generatefield(self.settings.extratiles, windowsize)
         self.lenghtoftravel = len(self.playingfield.fieldTiles)
         print(self.lenghtoftravel)
-        win.setworldcoordinates(0, 0, self.playingfield.canvassize[1], self.playingfield.canvassize[0])
+        win.setworldcoordinates(
+            0, 0, self.playingfield.canvassize[1], self.playingfield.canvassize[0])
 
         for i in range(len(self.playingfield.decoration)):
-            Renderer().render((102, 102, 102), self.playingfield.decoration[i], self.playingfield.dotsize, "plus")
+            Renderer().render((102, 102, 102),
+                              self.playingfield.decoration[i], self.playingfield.dotsize, "plus")
 
         for i in range(self.settings.playeramount):
             player = Player(self.settings.piececolors[i],
@@ -70,13 +86,15 @@ class GameMaster:
 
         for i in range(self.settings.playeramount):
             for x in range(self.settings.pieceamount):
-                self.players[i].playerPieces[x].placepiece(self.playingfield.starthouses[i][x].tileCoords)
+                self.players[i].playerPieces[x].placepiece(
+                    self.playingfield.starthouses[i][x].tileCoords)
 
         Renderer().inithighlight(self.playingfield.dotsize)
 
         Renderer().refreshui(self.playingfield.canvassize[1],
                              self.settings.playernames[self.playerchoice],
-                             self.piecechoice, self.roll, int(windowsize / fontsizefactor),
+                             self.piecechoice, self.roll, int(
+                                 windowsize / fontsizefactor),
                              Settings.playernames)
         position = self.players[self.playerchoice].playerPieces[self.piecechoice].position
         Renderer().highlight((position[0] + 0.5, position[1] + 0.5))
@@ -89,10 +107,12 @@ class GameMaster:
             for x in range(self.settings.pieceamount):
                 if x != 0:
                     piecetoshuffle = self.players[i].playerPieces[x]
-                    placement = random.randrange(0, len(self.playingfield.fieldTiles))
+                    placement = random.randrange(
+                        0, len(self.playingfield.fieldTiles))
 
                     if self.playingfield.fieldTiles[placement].tilestandingplayer is None:
-                        piecetoshuffle.movepiece(self.playingfield.fieldTiles[placement].tileCoords)
+                        piecetoshuffle.movepiece(
+                            self.playingfield.fieldTiles[placement].tileCoords)
                         self.playingfield.fieldTiles[placement].tilestandingplayer = i
                         self.playingfield.fieldTiles[placement].tilestandingpiece = x
 
@@ -105,23 +125,27 @@ class GameMaster:
 
     # Method for placing piece on the field from the start house
     def initiatepiece(self):
-
+        self.currentGameState = GAMESTATE_AWAIT
         piece = self.players[self.playerchoice].playerPieces[self.piecechoice]
 
         startingtile = self.playingfield.fieldTiles[self.playingfield.starttileids[self.playerchoice]]
 
         if startingtile.tilestandingplayer is self.playerchoice:
-            return
+            return False
 
         piece.positioninplayingfield = self.playingfield.starttileids[self.playerchoice]
         piece.tilesmoved = 1
-        piece.movepiece(self.playingfield.fieldTiles[self.playingfield.starttileids[self.playerchoice]].tileCoords)
+        piece.movepiece(
+            self.playingfield.fieldTiles[self.playingfield.starttileids[self.playerchoice]].tileCoords)
         if startingtile.tilestandingplayer is not None:
-            self.kickpiece(startingtile.tilestandingplayer, startingtile.tilestandingpiece)
+            self.kickpiece(startingtile.tilestandingplayer,
+                           startingtile.tilestandingpiece)
         self.playingfield.fieldTiles[
             self.playingfield.starttileids[self.playerchoice]].tilestandingplayer = self.playerchoice
         self.playingfield.fieldTiles[
             self.playingfield.starttileids[self.playerchoice]].tilestandingpiece = self.piecechoice
+
+        return True
 
     # Method for moving pieces on the playing field
     def iteratetroughfield(self, looprange):
@@ -130,7 +154,8 @@ class GameMaster:
         for i in range(looprange):
             piece.tilesmoved += 1
             piece.positioninplayingfield += 1
-            piece.movepiece(self.playingfield.fieldTiles[piece.positioninplayingfield].tileCoords)
+            piece.movepiece(
+                self.playingfield.fieldTiles[piece.positioninplayingfield].tileCoords)
 
     # Method for moving pieces inside end houses
     def iteratetroughhouse(self, looprange):
@@ -138,7 +163,8 @@ class GameMaster:
 
         for i in range(looprange):
             piece.positioninhouse += 1
-            piece.movepiece(self.playingfield.endhouses[self.playerchoice][piece.positioninhouse].tileCoords)
+            piece.movepiece(
+                self.playingfield.endhouses[self.playerchoice][piece.positioninhouse].tileCoords)
 
     def notifytileofpeice(self):
         piece = self.players[self.playerchoice].playerPieces[self.piecechoice]
@@ -164,87 +190,94 @@ class GameMaster:
 
     # Method with logic and rules for moving pieces.
     def performmovement(self, roll):
+        self.currentGameState = GAMESTATE_AWAIT
 
         piece = self.players[self.playerchoice].playerPieces[self.piecechoice]
 
         # If the move results in a piece going outside the playing field or end house, the move gets skipped
         if piece.tilesmoved + roll + piece.positioninhouse > self.lenghtoftravel - 1 + self.settings.pieceamount:
-            return False
+            return MOVESTATE_OUTOFBOUNDS
 
-        # # If the move results in the piece exceeding the lenght of travel needed to get into a house
-        # if piece.tilesmoved + roll > self.lenghtoftravel:
-        #     currenttilesmoved = piece.tilesmoved
-        #     housedifference = self.lenghtoftravel - piece.tilesmoved
-        #     if piece.isinhouse is True:
-        #         currentposition = self.playingfield.endhouses[self.playerchoice][piece.positioninhouse]
-        #     else:
-        #         currentposition = self.playingfield.fieldTiles[piece.positioninplayingfield]
-        #
-        #     futureposition = self.playingfield.endhouses[
-        #         self.playerchoice][piece.positioninhouse + (roll - housedifference)]
-        #
-        #     # if the tile inside the house is already occupied, the move gets skipped
-        #     if futureposition.tilestandingpiece is not None:
-        #         self.iteratetroughfield(housedifference)
-        #         self.iteratetroughhouse(roll - housedifference)
-        #         piece.tilesmoved = currenttilesmoved
-        #         if piece.isinhouse is False:
-        #             piece.positioninplayingfield = currentposition.tileID
-        #             piece.positioninhouse = -1
-        #             piece.movepiece(currentposition.tileCoords)
-        #         else:
-        #             piece.positioninhouse = currentposition.tileID
-        #             piece.movepiece(currentposition.tileCoords)
-        #
-        #         return False
-        #
-        #     self.removepiecefromhouse()
-        #     self.iteratetroughfield(housedifference)
-        #     self.iteratetroughhouse(roll - housedifference)
-        #     self.notifyhouseofpiece()
-        #     if piece.isinhouse is False:
-        #         self.playingfield.fieldTiles[piece.positioninplayingfield].tilestandingplayer = None
-        #     piece.isinhouse = True
-        #     return False
+        # If the move results in the piece exceeding the lenght of travel needed to get into a house
+        if piece.tilesmoved + roll > self.lenghtoftravel:
+            currenttilesmoved = piece.tilesmoved
+            housedifference = self.lenghtoftravel - piece.tilesmoved
+            if piece.isinhouse is True:
+                currentposition = self.playingfield.endhouses[self.playerchoice][piece.positioninhouse]
+            else:
+                currentposition = self.playingfield.fieldTiles[piece.positioninplayingfield]
+        
+            futureposition = self.playingfield.endhouses[self.playerchoice][piece.positioninhouse + (roll - housedifference)]
+        
+            # if the tile inside the house is already occupied, the move gets skipped
+            if futureposition.tilestandingpiece is not None:
+                self.iteratetroughfield(housedifference)
+                self.iteratetroughhouse(roll - housedifference)
+                piece.tilesmoved = currenttilesmoved
+                if piece.isinhouse is False:
+                    piece.positioninplayingfield = currentposition.tileID
+                    piece.positioninhouse = -1
+                    piece.movepiece(currentposition.tileCoords)
+                else:
+                    piece.positioninhouse = currentposition.tileID
+                    piece.movepiece(currentposition.tileCoords)
+        
+                return MOVESTATE_TILEOCCUPIED
+        
+            self.removepiecefromhouse()
+            self.iteratetroughfield(housedifference)
+            self.iteratetroughhouse(roll - housedifference)
+            self.notifyhouseofpiece()
+            if piece.isinhouse is False:
+                self.playingfield.fieldTiles[piece.positioninplayingfield].tilestandingplayer = None
+            piece.isinhouse = True
+            return MOVESTATE_SUCCESS
 
         self.removepiecefromtile()
-        difference = (piece.positioninplayingfield + roll) - (self.lenghtoftravel - 1)
+        difference = (piece.positioninplayingfield + roll) - \
+            (self.lenghtoftravel - 1)
         difference = max(0, difference)
-        # currenttilesmoved = piece.tilesmoved
-        # currentposition = self.playingfield.fieldTiles[piece.positioninplayingfield]
-        #
-        # if difference > 0:
-        #     futureposition = self.playingfield.fieldTiles[difference - 1]
-        #
-        # else:
-        #     futureposition = self.playingfield.fieldTiles[piece.positioninplayingfield + roll]
+        currenttilesmoved = piece.tilesmoved
+        currentposition = self.playingfield.fieldTiles[piece.positioninplayingfield]
+        
+        if difference > 0:
+            futureposition = self.playingfield.fieldTiles[difference - 1]
+        
+        else:
+            futureposition = self.playingfield.fieldTiles[piece.positioninplayingfield + roll]
 
-        # # if the tile inside the playing field is occupied by a piece of the same player, the move gets skipped
-        # if futureposition.tilestandingplayer == self.playerchoice:
-        #     self.iteratetroughfield(roll - difference)
-        #     if difference > 0:
-        #         piece.positioninplayingfield = -1
-        #         self.iteratetroughfield(difference)
-        #     piece.tilesmoved = currenttilesmoved
-        #     piece.positioninplayingfield = currentposition.tileID
-        #     piece.movepiece(currentposition.tileCoords)
-        #     self.notifytileofpeice()
-        #     return False
-        #
-        # # if the tile inside the playing field is occupied by a piece of another player, the peice gets kicked out
-        # elif futureposition.tilestandingplayer != self.playerchoice and futureposition.tilestandingplayer is not None:
-        #     self.iteratetroughfield(roll - difference)
-        #     if difference > 0:
-        #         piece.positioninplayingfield = -1
-        #         self.iteratetroughfield(difference)
-        #     self.kickpiece(futureposition.tilestandingplayer, futureposition.tilestandingpiece)
-        #     self.notifytileofpeice()
-        #     return True
+        # if the tile inside the playing field is occupied by a piece of the same player, the move gets skipped
 
         self.iteratetroughfield(roll - difference)
         if difference > 0:
             piece.positioninplayingfield = -1
             self.iteratetroughfield(difference)
 
+        if futureposition.tilestandingplayer == self.playerchoice:
+            piece.tilesmoved = currenttilesmoved
+            piece.positioninplayingfield = currentposition.tileID
+            piece.movepiece(currentposition.tileCoords)
+            self.notifytileofpeice()
+            return MOVESTATE_TILEOCCUPIED
+        
+        # if the tile inside the playing field is occupied by a piece of another player, the peice gets kicked out
+        elif futureposition.tilestandingplayer != self.playerchoice:
+            if futureposition.tilestandingplayer is None:
+                self.notifytileofpeice()
+                return MOVESTATE_SUCCESS
+
+            if futureposition.tileID == self.playingfield.starttileids[futureposition.tilestandingplayer]:
+                piece.movepiece(currentposition.tileCoords)
+                piece.positioninplayingfield = currentposition.tileID
+                self.notifytileofpeice()
+                return MOVESTATE_TILEOCCUPIED
+
+            self.kickpiece(futureposition.tilestandingplayer, futureposition.tilestandingpiece)
+            self.notifytileofpeice()
+            return MOVESTATE_SUCCESS
+
         self.notifytileofpeice()
-        return True
+        return MOVESTATE_SUCCESS
+
+    def refreshui(self):
+        Renderer().refreshui(self.playingfield.canvassize[1], self.settings.playernames[self.playerchoice], self.piecechoice, self.roll, gamefont[1], self.settings.playernames)
--- source/inputs.py
+++ source/inputs.py
@@ -3,8 +3,6 @@ from tkinter import *
 
 
 class InputSystem:
-    gamefont = ('Arial', int(windowsize / fontsizefactor), 'normal')
-
     master = None
     # master = GameMaster(4, 4, 3)
     # master.inmenu = False
@@ -13,11 +11,17 @@ class InputSystem:
         win.onkey(lambda key="Return": self.inputhandler(key), "Return")
         win.onkey(lambda key="Space": self.inputhandler(key), "space")
         win.onkey(lambda key="Left": self.inputhandler(key), "Left")
-        win.onkey(lambda key="Right": self.inputhandler(key), "Right")
+        win.onkey(lambda key="1": self.inputhandler(key), "1")
+        win.onkey(lambda key="2": self.inputhandler(key), "2")
+        win.onkey(lambda key="3": self.inputhandler(key), "3")
+        win.onkey(lambda key="4": self.inputhandler(key), "4")
+        win.onkey(lambda key="5": self.inputhandler(key), "5")
+        win.onkey(lambda key="6": self.inputhandler(key), "6")
+        win.onkey(lambda key="p": self.inputhandler(key), "p")
         rollbtn = Button(canvas.master, text="Roll", command=lambda key="Space": self.inputhandler(key),
-                         font=self.gamefont)
+                         font=gamefont)
         movebtn = Button(canvas.master, text="Move", command=lambda key="Return": self.inputhandler(key),
-                         font=self.gamefont)
+                         font=gamefont)
         rollbtn.place(relx=0.7, rely=0.05, anchor=CENTER)
         movebtn.place(relx=0.3, rely=0.05, anchor=CENTER)
 
@@ -33,10 +37,7 @@ class InputSystem:
             if position == piecelist[pi].position:
                 self.master.piecechoice = pi
                 Renderer().highlight((position[0] + 0.5, position[1] + 0.5))
-                Renderer().refreshui(self.master.playingfield.canvassize[1],
-                                     self.master.settings.playernames[self.master.playerchoice],
-                                     self.master.piecechoice, self.master.roll, self.gamefont[1],
-                                     self.master.settings.playernames)
+                self.master.refreshui()
                 return
 
     def cyclepiecesright(self):
@@ -60,91 +61,155 @@ class InputSystem:
         print(self.master.piecechoice)
 
     def roll(self):
-        if self.master.roll == 0:
-            # self.master.roll = random.randrange(1, 7)
-            self.master.roll = 6
+        if self.master.currentGameState != GAMESTATE_MOVE:
+            self.master.roll = random.randrange(1, 7)
+            self.master.refreshui()
+
+        if self.master.currentGameState != GAMESTATE_GETPIECEOUT:
+            self.master.currentGameState = GAMESTATE_MOVE
+
+    def debugroll(self, roll):
+        if self.master.currentGameState != GAMESTATE_MOVE:
+            self.master.roll = roll
+            self.master.refreshui()
+
+        if self.master.currentGameState != GAMESTATE_GETPIECEOUT:
+            self.master.currentGameState = GAMESTATE_MOVE
 
     def cycleplayers(self):
+        # player = self.master.players[self.master.playerchoice]
+        #
+        # for i in player.playerPieces:
+        #     if i.isinhouse is False:
+        #         break
+        #     print(f'{self.master.settings.playernames[self.master.playerchoice]} Won!')
+
         if self.master.playerchoice + 1 > self.master.settings.playeramount - 1:
             self.master.playerchoice = 0
         else:
             self.master.playerchoice += 1
 
+        if self.ispieceonboard():
+            self.master.currentGameState = GAMESTATE_ROLL
+        else:
+            self.master.currentGameState = GAMESTATE_GETPIECEOUT
+
+        self.master.roll = 0
+        self.master.attempt = 0
+
         pos = self.master.players[self.master.playerchoice].playerPieces[self.master.piecechoice].position
         Renderer().highlight((pos[0] + 0.5, pos[1] + 0.5))
 
     def inputhandler(self, key):
-        if self.master.inmenu is False:
+        if self.master.inmenu is False and self.master.currentGameState != GAMESTATE_AWAIT:
             match key:
                 case "Left":
                     self.cyclepiecesleft()
                 case "Right":
                     self.cyclepiecesright()
                 case "Return":
-                    self.moveattempt()
+                    if self.master.roll == 0:
+                        return
+
+                    self.moveattempt(self.master.players[self.master.playerchoice].playerPieces[self.master.piecechoice])
                 case "Space":
                     self.roll()
+                    if self.master.currentGameState == GAMESTATE_GETPIECEOUT:
+                        self.moveattempt(self.master.players[self.master.playerchoice].playerPieces[self.master.piecechoice])
 
-            Renderer().refreshui(self.master.playingfield.canvassize[1],
-                                 self.master.settings.playernames[self.master.playerchoice],
-                                 self.master.piecechoice, self.master.roll, self.gamefont[1],
-                                 self.master.settings.playernames)
-
-    def moveattempt(self):
-        if self.master.roll != 0:
-            piecesonboard = None
-
-            piece = self.master.players[self.master.playerchoice].playerPieces[self.master.piecechoice]
-
-            for i in range(len(self.master.players[self.master.playerchoice].playerPieces)):
-                if self.master.players[self.master.playerchoice].playerPieces[i].positioninplayingfield is not None:
-                    piecesonboard = True
-                elif piecesonboard is not True:
-                    piecesonboard = False
-
-            print("piece on board is ", piecesonboard)
-
-            if piecesonboard is False and self.master.roll == 6:
-                self.master.initiatepiece()
-                self.cycleplayers()
-                self.master.roll = 0
-                self.master.attempt = 0
-                print("Placed piece of playing field")
-                return
-
-            elif piecesonboard is False and self.master.roll != 6:
-                if self.master.attempt + 1 > 2:
-                    self.master.roll = 0
-                    self.master.attempt = 0
+                case "1":
+                    self.debugroll(1)
+                case "2":
+                    self.debugroll(2)
+                case "3":
+                    self.debugroll(3)
+                case "4":
+                    self.debugroll(4)
+                case "5":
+                    self.debugroll(5)
+                case "6":
+                    self.debugroll(6)
+                case "p":
                     self.cycleplayers()
-                    print("Out of attempts, turn skipped")
-                else:
-                    self.master.roll = 0
-                    self.master.attempt += 1
-                    print("Didnt throw 6, you have another attempt")
-                return
+        
+        self.master.refreshui()
 
-            if piece.positioninplayingfield is None and self.master.roll == 6:
+    def ispieceonboard(self):
+        for i in range(len(self.master.players[self.master.playerchoice].playerPieces)):
+            if self.master.players[self.master.playerchoice].playerPieces[i].positioninplayingfield is not None:
+                return True
+
+        return False
+
+    def canpiecemove(self, roll, piece):
+        if piece.positioninplayingfield is None:
+            return False
+        if self.master.lenghtoftravel - piece.tilesmoved > roll and piece.positioninhouse == -1:
+            print(f'PIECE CAN STILL TRAVEL ON FIELD {self.master.lenghtoftravel - piece.tilesmoved} GREATER THAN {roll}')
+            return True
+        if piece.positioninhouse + roll < self.master.settings.pieceamount:
+            print(f'PIECE CAN STILL TRAVEL IN HOUSES {piece.positioninhouse + roll} LESS THAN {self.master.settings.pieceamount}')
+            return True
+        if piece.tilesmoved + roll + piece.positioninhouse < self.master.lenghtoftravel - 1 + self.master.settings.pieceamount:
+            return True
+
+        return False
+    
+    def cananypiecemove(self, roll):
+        availablemoves = 0
+
+        for i in range(len(self.master.players[self.master.playerchoice].playerPieces)):
+            piece = self.master.players[self.master.playerchoice].playerPieces[i]
+
+            if piece.positioninplayingfield is None and roll == 6 or self.canpiecemove(roll, piece):
+                availablemoves += 1
+
+        return availablemoves
+
+    def moveattempt(self, piece):
+        if self.master.currentGameState == GAMESTATE_GETPIECEOUT:
+            if self.master.roll == 6:
                 self.master.initiatepiece()
                 self.cycleplayers()
-                self.master.roll = 0
-                print("Placed piece of playing field")
-                return
 
-            elif piece.positioninplayingfield is None and self.master.roll != 6:
-                print("Didnt roll a 6, choose a different piece or skip turn")
-                return
-
-            if self.master.performmovement(self.master.roll) is True:
+            elif self.master.attempt + 1 > 2:
                 self.cycleplayers()
-                self.master.roll = 0
-                print("Moved piece")
-                return
-            if self.master.performmovement(self.master.roll) is False:
-                print("Piece cant move there, choose a different piece or skip turn")
-                return
-            print("something went wrong")
 
+            else:
+                self.master.attempt += 1
+            return
+
+        elif piece.positioninplayingfield is None and self.master.currentGameState == GAMESTATE_MOVE:
+            if self.master.roll == 6:
+                if self.master.initiatepiece():
+                    self.cycleplayers()
+                    return
+                self.master.currentGameState = GAMESTATE_MOVE
+                return
+        
+        availablemoves = self.cananypiecemove(self.master.roll)
+        print(f'Available moves is {availablemoves}')
+
+        if availablemoves <= 0:
+            self.cycleplayers()
+            return
+
+        if not self.canpiecemove(self.master.roll, piece):
+            return
+        
+        result = self.master.performmovement(self.master.roll)
+        print(f'Result of move is {result}')
+
+        match result:
+            case 0:  # MOVESTATE_SUCCESS
+                self.cycleplayers()
+            case 1:  # MOVESTATE_OUTOFBOUNDS
+                self.cycleplayers()
+            case 2:  # MOVESTATE_TILEOCCUPIED
+                if availablemoves <= 1:
+                    self.cycleplayers()
+                    return
+                self.master.currentGameState = GAMESTATE_MOVE
 
 # inputsystem = InputSystem()
 # win.mainloop()
